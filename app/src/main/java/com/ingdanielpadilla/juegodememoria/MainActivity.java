d93e4a49d79ea963459332b6740e917f84780184
Daniel Padilla <danperzipa@hotmail.com>
2015-08-14 23:24:30 -0500

    Variables declaradas con for.

--- app/src/main/java/com/ingdanielpadilla/juegodememoria/MainActivity.java
+++ app/src/main/java/com/ingdanielpadilla/juegodememoria/MainActivity.java
@@ -2,6 +2,7 @@ package com.ingdanielpadilla.juegodememoria;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,6 +11,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.os.Handler;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,46 +21,35 @@ public class MainActivity extends AppCompatActivity {
     Integer codigo=0;
     Integer puntos=0;
     Integer wait=0;
-    Button b;
+    Button bi;
     Button bf;
+    Button[] b=new Button[17];;
     TextView t1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         t1=(TextView) findViewById(R.id.t1);
-        Button b1=(Button) findViewById(R.id.b1);
-        Button b2=(Button) findViewById(R.id.b2);
-        Button b3=(Button) findViewById(R.id.b3);
-        Button b4=(Button) findViewById(R.id.b4);
-        Button b5=(Button) findViewById(R.id.b5);
-        Button b6=(Button) findViewById(R.id.b6);
-        Button b7=(Button) findViewById(R.id.b7);
-        Button b8=(Button) findViewById(R.id.b8);
-        Button b9=(Button) findViewById(R.id.b9);
-        Button b10=(Button) findViewById(R.id.b10);
-        Button b11=(Button) findViewById(R.id.b11);
-        Button b12=(Button) findViewById(R.id.b12);
-        Button b13=(Button) findViewById(R.id.b13);
-        Button b14=(Button) findViewById(R.id.b14);
-        Button b15=(Button) findViewById(R.id.b15);
-        Button b16=(Button) findViewById(R.id.b16);
-        b1.setTag(1);
-        b2.setTag(6);
-        b3.setTag(7);
-        b4.setTag(6);
-        b5.setTag(2);
-        b6.setTag(5);
-        b7.setTag(8);
-        b8.setTag(4);
-        b9.setTag(5);
-        b10.setTag(4);
-        b11.setTag(8);
-        b12.setTag(3);
-        b13.setTag(3);
-        b14.setTag(7);
-        b15.setTag(2);
-        b16.setTag(1);
+
+        for (int i=1;i<=16;i++){
+            String res="b"+i;
+            Integer cod=getResources().getIdentifier(res, "id", getPackageName());
+            Log.v("Desarrollo",cod.toString());
+            b[i]=(Button) findViewById(cod);
+        }
+
+
+        ArrayList<Integer> number = new ArrayList<Integer>();
+        for (int i = 1; i <= 16; ++i) {
+            number.add(i/2);
+            b[i].setTag(1);
+        }
+        Collections.shuffle(number);
+        for (int i = 1; i <= 16; ++i) {
+            b[i].setTag(number.get(i-1));
+        }
+
 
 
 
@@ -95,19 +88,19 @@ public class MainActivity extends AppCompatActivity {
     public void VolteaBoton(View view) {
 
         if (wait==0){
-            b = (Button) view;
-            codigo = b.getId();
+            bi = (Button) view;
+            codigo = bi.getId();
         }
         if (!codigo.equals(anterior)&& wait==0){
-            Integer tag=(Integer)b.getTag();
-            b.setText(tag.toString());
+            Integer tag=(Integer)bi.getTag();
+            bi.setText(tag.toString());
             if(anterior==0){
             anterior=codigo;
             }else{
                 bf=(Button) findViewById(anterior);
                 if (tag==(Integer)bf.getTag()){
                     Toast.makeText(this,"Exelente",Toast.LENGTH_SHORT).show();
-                    b.setEnabled(false);
+                    bi.setEnabled(false);
                     bf.setEnabled(false);
                     puntos=puntos+1;
 
@@ -120,7 +113,7 @@ public class MainActivity extends AppCompatActivity {
                     Handler del= new Handler();
                         del.postDelayed(new Runnable() {
                             public void run() {
-                                b.setText("Logo");
+                                bi.setText("Logo");
                                 bf.setText("Logo");
                                 wait=0;
                             }
